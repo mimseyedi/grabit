94f1f7342a42412cf39e8fa66385a0a86c12dece
Mohammad <mim.seyedi@gmail.com>
2024-08-26 15:44:22 +0330

    HandMemory class, implemented.

--- grabit/memory.py
+++ grabit/memory.py
@@ -91,3 +91,51 @@ class StackMemory:
             f"path='{self.path}', "
             f"size={self.size})"
         )
+
+
+class HandMemory:
+    def __init__(self, hand) -> None:
+        self._hand = hand
+        self._path = Path(
+            hand.handdir,
+            f'.{hand.name}.db',
+        )
+
+    @property
+    def hand(self):
+        return self._hand
+
+    @property
+    def path(self):
+        return self._path
+
+    def build(self) -> None:
+        self.path.touch(exist_ok=True)
+
+    def read(self) -> dict:
+        with open(
+            file=self.path,
+            mode='rb',
+        ) as dbfile:
+            db = pickle.load(dbfile)
+        return db
+
+    def write(self, db) -> None:
+        with open(
+            file=self.path,
+            mode='wb',
+        ) as dbfile:
+            pickle.dump(db, dbfile)
+
+    def __iter__(self) -> Iterator:
+        yield from self.read().items()
+
+    def __len__(self) -> int:
+        return len(self.read())
+
+    def __repr__(self) -> str:
+        return (
+            f"HandDB("
+            f"hand={self.hand.name}, "
+            f"path='{self.path}')"
+        )
